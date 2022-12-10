1cc20d99d9b9f7d5c8b88e158a5fadd82c17de04
jim0112 <b08902015@csie.ntu.edu.tw>
2022-12-10 12:46:50 +0800

    change tetst.js

--- frontend/src/container/TestPage/Test.js
+++ frontend/src/container/TestPage/Test.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Box } from '@mui/material';
+import { Box, Button, Menu, MenuItem, Typography, List, ListItem, ListItemText, Select, FormControl, InputLabel } from '@mui/material';
 import '../../css/LearnSets.css'
 import axios from 'axios'
-import FolderSpecialRoundedIcon from '@mui/icons-material/FolderSpecialRounded';
-import { v4 as uuidv4 } from 'uuid';
 import { useNavigate } from 'react-router-dom';
 
 const instance = axios.create({
@@ -11,17 +9,37 @@ const instance = axios.create({
 })
 
 const Test = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
     const [learnSets, setlearnSets] = useState([]);
+    const [selectedIndex, setSelectedIndex] = useState(1);
+    const [amount, setAmount] = useState(0);
     
+    const handleClickListItem = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleMenuItemClick = (event, index) => {
+        setSelectedIndex(index);
+        setAnchorEl(null);
+    };
+
+    const handleAmountChange = (e) => {
+        setAmount(e.target.value);
+    }
+
     const findLearnSets = async() => {
         const { data: { msg, contents } } = await instance.get('/lectures');
-        setlearnSets(contents);
-    }
+        setlearnSets(contents.map((item) => item.name));
+    };
 
     const navigate = useNavigate();
     const navigateToCards = (name) => {
         navigate('/test/' + name);
-    }
+    };
 
     useEffect(() => {
         findLearnSets();
@@ -29,21 +47,60 @@ const Test = () => {
 
     return (
         <>
-            <div className='learnsetsContainer'>
-                {
-                    learnSets.map(item => (
-                        <>
-                            <div className='learnSet' key={uuidv4()} >
-                                <FolderSpecialRoundedIcon 
-                                    className='folderIcon' 
-                                    onClick={() => navigateToCards(item.name)}
-                                />
-                                <Box className='name'>{item.name}</Box>
-                            </div>
-                        </>
-                    ))
-                }
-            </div>
+            <Box>
+                <Typography>選擇要考試的學習集</Typography>
+                <List component="nav" aria-label="Device settings">
+                    <ListItem
+                      button
+                      aria-haspopup="true"
+                      aria-controls="lock-menu"
+                      aria-label="選擇學習集"
+                      onClick={handleClickListItem}
+                    >
+                        <ListItemText
+                            primary="學習集"
+                            secondary={learnSets[selectedIndex]}
+                        />
+                    </ListItem>
+                </List>
+                <Menu
+                    id="lock-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    {learnSets.map((option, index) => (
+                        <MenuItem
+                            key={option}
+                            selected={index === selectedIndex}
+                            onClick={(event) => handleMenuItemClick(event, index)}
+                        >
+                            {option}
+                        </MenuItem>
+                    ))}
+                </Menu>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">題數</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={amount}
+                        label="Amount"
+                        onChange={handleAmountChange}
+                    >
+                        <MenuItem value={5}>5</MenuItem>
+                        <MenuItem value={10}>10</MenuItem>
+                        <MenuItem value={15}>15</MenuItem>
+                        <MenuItem value={20}>20</MenuItem>
+                        <MenuItem value={25}>25</MenuItem>
+                        <MenuItem value={30}>30</MenuItem>
+                        <MenuItem value={35}>35</MenuItem>
+                        <MenuItem value={40}>40</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button onClick={() => navigateToCards(learnSets[selectedIndex], amount)}>確定</Button>
+            </Box>
         </>
     );
 }
