70fce8a6c00141721e8a61d4b9016ea5b49658e3
alterdra <bennybenny232323@gmail.com>
2022-12-28 22:32:26 +0800

    Some muiIcon and css adjustment

--- frontend/src/components/LoginBar.js
+++ frontend/src/components/LoginBar.js
@@ -1,8 +1,18 @@
 import { useState } from 'react';
 import { useUserName } from '../container/hook/useUserName';
-import { Box, InputAdornment, TextField, IconButton, Button } from '@mui/material'
+import { Box, InputAdornment, TextField, IconButton, Button, Fab } from '@mui/material'
 import { AccountCircle, VisibilityOff , Visibility } from '@mui/icons-material'
 import RegisterModal from './modals/RegisterModal';
+
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import InputIcon from '@mui/icons-material/Input';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import LogoutIcon from '@mui/icons-material/Logout';
+import LoginIcon from '@mui/icons-material/Login';
 
 const LoginBar = () => {
     const { user, setUser, password, setPassword, 
@@ -22,8 +32,12 @@ const LoginBar = () => {
 	
     return (
         <>
-            <div>請註冊帳號</div>
-            <Button onClick={() => handleOpen()}>註冊帳號</Button>
+            <ListItem>
+                <Fab variant="extended"  onClick={() => handleOpen()}>
+                    <HowToRegIcon sx={{ mr: 1 }} />
+                    點選以註冊帳號
+                </Fab>
+            </ListItem>
             <RegisterModal 
                 showModal={showModal}
                 userName={newUser} changeUserName={changeNewUser}
@@ -31,56 +45,82 @@ const LoginBar = () => {
                 handleCreateUser={handleRegister} handleClose={handleClose}
             />
             {!signedIn ? 
-            (<>
-                <div>請登入帳號密碼</div>
-                <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <TextField
-                    id="input-with-icon-textfield1"
-                    label="Username"
-                    InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                        <AccountCircle />
-                        </InputAdornment>
-                    ),
-                    }}
-                    value={user}
-                    onChange={(e) => setUser(e.target.value)}
-                    variant="standard"
-                />
-                <TextField
-                    type={showPassword ? 'text' : 'password'}
-                    id="input-with-icon-textfield2"
-                    label="Password"
-                    InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                        <AccountCircle />
-                        </InputAdornment>
-                    ),
-                    endAdornment: (
-                        <InputAdornment position="end">
-                        <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                            edge="end"
-                        >
-                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                        </InputAdornment>
-                    ),
-                    }}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    variant="standard"
-                />
-                <Button onClick={() => handleLogin()}>登入</Button>
-                </Box>
+            (<>    
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <InputIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="請登入帳號密碼" />
+                </ListItem>
+                <ListItem>
+                    <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                        <TextField
+                            id="input-with-icon-textfield1"
+                            label="Username"
+                            InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                <AccountCircle />
+                                </InputAdornment>
+                            ),
+                            }}
+                            value={user}
+                            onChange={(e) => setUser(e.target.value)}
+                            variant="standard"
+                        />
+                        <br></br>
+                        <TextField
+                            type={showPassword ? 'text' : 'password'}
+                            id="input-with-icon-textfield2"
+                            label="Password"
+                            InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                <AccountCircle />
+                                </InputAdornment>
+                            ),
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                                </InputAdornment>
+                            ),
+                            }}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            variant="standard"
+                        />
+                        <br/>
+                        <Fab variant="extended"  onClick={() => handleLogin()}>
+                            <LoginIcon sx={{ mr: 1 }} />
+                            登入
+                        </Fab>
+                    </Box>
+                </ListItem>
             </>) : 
             (<div>
-                歡迎回來, {user}
-                <Button onClick={() => handleLogout()}>登出</Button>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <ThumbUpAltIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={`歡迎回來, ${user}!`} />
+                </ListItem>
+                <ListItem>
+                    <Fab variant="extended"  onClick={() => handleLogout()}>
+                        <LogoutIcon sx={{ mr: 1 }} />
+                        登出
+                    </Fab>
+                </ListItem>
             </div>)
         }</>
     );
--- frontend/src/container/Homepage.js
+++ frontend/src/container/Homepage.js
@@ -3,13 +3,15 @@ import LoginBar from '../components/LoginBar';
 import { useState } from 'react';
 import { useUserName } from './hook/useUserName';
 import React from 'react';
-import '../css/Homepage.css'
+import '../css/Homepage.css';
 
 const Homepage = () => {
 	return (
 		<div>
-			<LoginBar />
 			<div className='container'>
+				<div className='LoginBarWrapper'>
+					<LoginBar />
+				</div>
 				<div className='SliderWrapper' dir="rtl">
 					<Slider />
 				</div>
