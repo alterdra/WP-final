66a8c9d8bf43648e56018606b08611c2f5b9c7c6
alterdra <bennybenny232323@gmail.com>
2023-01-02 21:13:13 +0800

    data init try catch

--- backend/src/upload.js
+++ backend/src/upload.js
@@ -10,38 +10,43 @@ const validateUser = async (name) => {
 }
 
 const dataInit = async ({ userName }) => {
-    const user = await validateUser(userName);
-    if(!user) {
-        console.log("Not verified");
-        return;
-    }
-    for(const { name, cards } of Data){
-        const existing = user.learnSets.find(ele => ele.name === name);
-        if(!existing){
-            // Add Learnset
-            const user = await validateUser(userName);
-            console.log(user);
-            const newLearnSet = new LearnSet({ name });
-            await newLearnSet.save();
-            user.learnSets.push(newLearnSet);
-            await user.save();
-            console.log(`Learnset ${name} added.`);
+    try {
+        const user = await validateUser(userName);
+        if(!user) {
+            console.log("Not verified");
+            return;
+        }
+        for(const { name, cards } of Data){
+            const existing = user.learnSets.find(ele => ele.name === name);
+            if(!existing){
+                // Add Learnset
+                const user = await validateUser(userName);
+                console.log(user);
+                const newLearnSet = new LearnSet({ name });
+                await newLearnSet.save();
+                user.learnSets.push(newLearnSet);
+                await user.save();
+                console.log(`Learnset ${name} added.`);
 
-            //Add cards
-            console.log(user, name);
-            let learnSet = user.learnSets.find(ele => ele.name === name);
-            learnSet = await learnSet.populate(["cards"]);
-            console.log(learnSet);
-            for(const card of cards){
-                const newCard = new WordCard(card);
-                await newCard.save();
-                learnSet.cards.push(newCard);
+                //Add cards
+                console.log(user, name);
+                let learnSet = user.learnSets.find(ele => ele.name === name);
+                learnSet = await learnSet.populate(["cards"]);
+                console.log(learnSet);
+                for(const card of cards){
+                    const newCard = new WordCard(card);
+                    await newCard.save();
+                    learnSet.cards.push(newCard);
+                }
+                await learnSet.save();
             }
-            await learnSet.save();
+            else{
+                console.log(`The set ${name} already exists.` );
+            }
         }
-        else{
-            console.log(`The set ${name} already exists.` );
-        }
+    }
+    catch (err) {
+        console.log(`Fail to init data.`);
     }
 }
 
