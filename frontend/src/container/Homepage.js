3fe22c4f2ee425cbb499930000986b33c5c5139e
jim0112 <b08902015@csie.ntu.edu.tw>
2023-01-02 14:38:08 +0800

    add fortune card

--- frontend/src/container/Homepage.js
+++ frontend/src/container/Homepage.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
-import { Box, Paper } from '@mui/material';
+import { useSpring, animated } from 'react-spring'; 
+import { Box, Card, Paper, Typography, Button } from '@mui/material';
 import Slider from '../components/ImageSlider';
 import NavBar from '../components/NavBar';
 import Data from '../mottos.json';
 import '../css/Homepage.css';
 
 import { styled } from '@mui/material/styles';
+import { Stack } from '@mui/system';
+
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
@@ -19,32 +22,77 @@ const newData = Data.filter(e => e['中文'].length <= 8 && e['日文'].length <
 
 const Homepage = () => {
 	const [mottoIndex, setMottoIndex] = useState(0);
+	const [isFlip, setIsFlip] = useState(false);
+	// 大吉(0-5)、中吉(6-18)、小吉(19-38)、吉(39-60)、末吉(61-80)、凶(81-95)、大凶(96-100)
+	const [result, setResult] = useState("吉");
+	const { opacity, transform } = useSpring({
+		opacity: isFlip ? 1 : 0,
+		transform: `perspective(1200px) rotateY(${isFlip ? 0 : 180}deg)`
+	})
+
+	const lottery = () => {
+		let random = Math.floor(Math.random() * 101)
+		if(random >= 0 && random <=5)
+			setResult("大吉");
+		else if(random >= 6 && random <= 18)
+			setResult("中吉");
+		else if(random >= 19 && random <= 38)
+			setResult("小吉");
+		else if(random >= 39 && random <= 60)
+			setResult("吉");
+		else if(random >= 61 && random <= 80)
+			setResult("末吉");
+		else if(random >= 81 && random <= 95)
+			setResult("凶");
+		else if(random >= 96 && random <= 100)
+			setResult("大凶");
+	}
+
+	const handleFlip = () => {
+		setIsFlip(prev => !prev);
+		if(!isFlip)
+			lottery();
+	}
+	
 	useEffect(() => {
 		setInterval(() => {	
 			setMottoIndex(prev => (prev + 1) % newData.length);
 		}, 5000);
 	}, []);
-	
+
 	return (
 		<Box sx={{ display: 'flex' }}>
 			<NavBar />
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                 <DrawerHeader />
-				<div className='container'>
-					<div className='Text'>
-						<Paper className='chinese' variant="outlined" elevation={2}>	
-							{newData[mottoIndex]['中文']}
-						</Paper>
+				<Stack direction='row' position='relative'>
+					<div onClick={() => handleFlip()}>
+						<animated.div className='fortune' style={{ opacity, transform }}>
+							<Typography sx={{ fontSize: '30px' }}>{result}</Typography>
+						</animated.div>
+						<animated.div className='fortune' style={{
+							opacity: opacity.to(o => 1 - o),
+							transform: transform.to(t => `${t} rotateY(180deg)`)
+						}}>
+							<Typography sx={{ fontSize: '30px' }}>今日の運勢</Typography>
+						</animated.div>
 					</div>
-					<div className='SliderWrapper'>
-						<Slider />
+					<div className='container'>
+						<div className='Text'>
+							<Paper className='chinese' variant="outlined" elevation={2}>	
+								{newData[mottoIndex]['中文']}
+							</Paper>
+						</div>
+						<div className='SliderWrapper'>
+							<Slider />
+						</div>
+						<div className='Text'>
+							<Paper className='japanese' variant="outlined" elevation={2}>
+								{newData[mottoIndex]['日文']}
+							</Paper>
+						</div>
 					</div>
-					<div className='Text'>
-						<Paper className='japanese' variant="outlined" elevation={2}>
-							{newData[mottoIndex]['日文']}
-						</Paper>
-					</div>
-				</div>
+				</Stack>
 			</Box>
 		</Box>
 	);
